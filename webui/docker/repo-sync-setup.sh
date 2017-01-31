4f785eb313fbca5841cedbfba34e7b64a4cf7c4c
Pavel Kulakovsky <pkulakovsky@gmail.com>
2017-01-31 07:17:23 +0200

    added web ui added kubernetes configuration yaml files added docker image

---
+++ webui/docker/repo-sync-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+${NGINX_LOCATION}
+
+cd ${NGINX_DATA_LOCATION}
+
+rm -rf *
+
+git init
+git remote add origin -f ${GIT_REPO_URL}
+git config core.sparseCheckout true
+echo ${WEBUI_PATH} >> .git/info/sparse-checkout
+git pull origin ${GIT_BRANCH}
+
+if [ ${GIT_BRANCH} ]
+then
+    git checkout ${GIT_BRANCH}
+fi
+
+i=1
+while [ ${i} ];
+do
+    git fetch
+    git pull origin ${GIT_BRANCH}
+
+    sleep ${GIT_REPO_SYNC_TIME}
+done
